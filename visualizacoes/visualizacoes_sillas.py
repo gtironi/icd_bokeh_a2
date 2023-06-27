8dc14458da4d214e35c9134a83f66dd8333db4a5
Sillas Rocha <125564261+scrocha@users.noreply.github.com>
2023-06-27 00:00:08 -0300

    fiz o servidor bokeh rodar
    agora o site possui iteratividade
    git commit -m "🙏"

--- visualizacoes/visualizacoes_sillas.py
+++ visualizacoes/visualizacoes_sillas.py
@@ -1,18 +1,25 @@
 # Importações do módulo de visualização
 from bokeh.io import save, show, output_file
 from bokeh.plotting import figure, curdoc
+from bokeh.layouts import column
+from bokeh.models import Select
 import read_data
+
+# data_spotify = read_data.csv_get_top("visualizacoes/data/spotify_youtube.csv",
+#                                      "Stream", duplicated_column = "Track")
+
+# names_spotify = read_data.csv_get_top_names("visualizacoes/data/spotify_youtube.csv",
+#                                             "Track", "Stream")
+
+all_data = read_data.csv_to_columndatasource("visualizacoes/data/spotify_youtube.csv")
+
 
 categories = ["Danceability", "Energy", 
               "Valence", "Speechiness", "Acousticness"]
 
-data_spotify = read_data.csv_get_top("visualizacoes/data/spotify_youtube.csv",
-                                     "Stream", duplicated_column = "Track")
+initial_category = categories[0]
 
-names_spotify = read_data.csv_get_top_names("visualizacoes/data/spotify_youtube.csv",
-                                            "Track", "Stream")
-
-all_data = read_data.csv_to_columndatasouce("visualizacoes/data/spotify_youtube.csv")
+filter_category = Select(title = "Categorias", value = initial_category, options = categories)
 
 output_file("testando.html")
 
@@ -24,16 +31,40 @@ output_file("testando.html")
 
 # show(spotify_plot)
 
+density_plot = figure(title = f"{initial_category} X Vezes tocadas no Spotify")
+
 histogram = read_data.histogram_count("visualizacoes/data/spotify_youtube.csv",
-                                      "Danceability", 10, proportion_column = "Stream")
+                                        initial_category, 10, proportion_column = "Stream")
 
-dance_plot = figure()
+density_plot.quad(top = histogram[0], bottom = 0,
+                    left = histogram[1][:-1],
+                    right = histogram[1][1:], fill_color = 'skyblue', fill_alpha = 0.7)
 
-dance_plot.quad(top = histogram[0], bottom = 0,
-                left = histogram[1][:-1],
-                right = histogram[1][1:], fill_color = 'skyblue', fill_alpha = 0.7)
-
-dance_plot.circle(x = "Danceability", y = "Stream", source = all_data)
+density_plot.circle(x = initial_category, y = "Stream", source = all_data)
 
 
-show(dance_plot)
+def update_density_plot(attr, old, new):
+    curdoc().clear()
+    category = filter_category.value
+
+    density_plot = figure(title = f"{category} X Vezes tocadas no Spotify")
+    
+    histogram = read_data.histogram_count("visualizacoes/data/spotify_youtube.csv",
+                                        category, 10, proportion_column = "Stream")
+
+    density_plot.quad(top = histogram[0], bottom = 0,
+                    left = histogram[1][:-1],
+                    right = histogram[1][1:], fill_color = 'skyblue', fill_alpha = 0.7)
+
+    density_plot.circle(x = category, y = "Stream", source = all_data)
+    
+    layout = column(filter_category, density_plot)
+
+    curdoc().add_root(layout)
+
+filter_category.on_change("value", update_density_plot)
+
+layout = column(filter_category, density_plot)
+
+curdoc().add_root(layout)
+
