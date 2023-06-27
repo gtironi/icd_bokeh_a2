df88ff0ba1f636dc9013f119416ec025b44c7ddc
Sillas Rocha <125564261+scrocha@users.noreply.github.com>
2023-06-26 22:32:15 -0300

    criei uma função para fazer histogramas
    criei uma função que usa o numpy e os parâmetros .quad de figuras do bokeh para gerar histogramas, e o usei para esboçar a densidade de ocorrências em um gráfico de scatter plot

--- visualizacoes/visualizacoes_sillas.py
+++ visualizacoes/visualizacoes_sillas.py
@@ -1,7 +1,5 @@
 # Importações do módulo de visualização
-from bokeh.palettes import Viridis256
 from bokeh.io import save, show, output_file
-from bokeh.models import LinearColorMapper, BasicTicker, ColorBar
 from bokeh.plotting import figure, curdoc
 import read_data
 
@@ -14,13 +12,28 @@ data_spotify = read_data.csv_get_top("visualizacoes/data/spotify_youtube.csv",
 names_spotify = read_data.csv_get_top_names("visualizacoes/data/spotify_youtube.csv",
                                             "Track", "Stream")
 
+all_data = read_data.csv_to_columndatasouce("visualizacoes/data/spotify_youtube.csv")
 
 output_file("testando.html")
 
-youtube_plot = figure(title = "Músicas mais ouvidas no Spotify", y_range = names_spotify,
-                      height = 400, width = 800)
+# spotify_plot = figure(title = "Músicas mais ouvidas no Spotify", y_range = names_spotify,
+#                       height = 300, width = 700)
 
-youtube_plot.hbar(y = "Track", right = "Stream",
-                  height = 0.8, source = data_spotify)
+# spotify_plot.hbar(y = "Track", right = "Stream",
+#                   height = 0.8, source = data_spotify)
 
-show(youtube_plot)
+# show(spotify_plot)
+
+histogram = read_data.histogram_count("visualizacoes/data/spotify_youtube.csv",
+                                      "Danceability", 10, proportion_column = "Stream")
+
+dance_plot = figure()
+
+dance_plot.quad(top = histogram[0], bottom = 0,
+                left = histogram[1][:-1],
+                right = histogram[1][1:], fill_color = 'skyblue', fill_alpha = 0.7)
+
+dance_plot.circle(x = "Danceability", y = "Stream", source = all_data)
+
+
+show(dance_plot)
