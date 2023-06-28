7c43dbb7f7704e3a21e4459ce5ca9efb53a9ad4a
Leonardo Alexandre <alexandreleonardo269@gmail.com>
2023-06-28 11:05:55 -0300

    Teste inicial

--- visualizacoes/visualizacoes_leonardo.py
+++ visualizacoes/visualizacoes_leonardo.py
@@ -3,3 +3,14 @@
 from read_data import csv_to_columndatasource
 from bokeh.plotting import figure
 from bokeh.io import output_file, save, show
+import pandas as pd
+
+# Teste
+
+df = pd.read_csv('visualizacoes/data/spotify_youtube_year.csv')
+
+plot_1 = figure()
+
+plot_1.circle(x = "Energy", y = "Danceability", source = df)
+
+show(plot_1)
