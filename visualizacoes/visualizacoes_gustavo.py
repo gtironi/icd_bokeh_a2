735b241259b177a8d2ae1c9a37a104dfb391fc8d
Sillas Rocha <125564261+scrocha@users.noreply.github.com>
2023-06-29 17:31:09 -0300

    Squashed commit of the following:
    commit 64aa69b3d9c7207c25362d1fc093464feff818f2
    Author: Gustavo Tironi <b51083@fgv.edu.br>
    Date:   Tue Jun 27 23:27:56 2023 -0300
    
        Outro rascunho para o plot 2
    
        Fiz outro scatterplot para depois escolher um deles
    
    commit 4a7de4c43039542e3d2a415da203af9de2c25465
    Author: Gustavo Tironi <b51083@fgv.edu.br>
    Date:   Tue Jun 27 21:56:08 2023 -0300
    
        Fiz o rascunho da 2 visualização
    
        Novamente, é apenas um rascunho, não está definida
        da forma correta, nem formatada.

--- visualizacoes/visualizacoes_gustavo.py
+++ visualizacoes/visualizacoes_gustavo.py
@@ -2,17 +2,12 @@
 import pandas as pd
 import numpy as np
 
-from bokeh.layouts import column
-from bokeh.models import ColumnDataSource, RangeTool
+from bokeh.layouts import column, row
+from bokeh.io import curdoc
+from bokeh.models import ColumnDataSource, RangeTool, OpenURL, TapTool, CustomJS
 from bokeh.plotting import figure, show
 
 df = pd.read_csv('visualizacoes/data/spotify_youtube_year.csv')
-
-df_views_por_artista = df.groupby('Artist')['Views'].sum().sort_values(ascending=False)
-
-df_views_por_track = df.groupby('Track')['Views'].sum().sort_values(ascending=False)
-
-df_100_mais_vistos = df_views_por_track.head(100)
 
 df.rename(columns={'release_date': 'year'}, inplace=True)
 
@@ -22,15 +17,17 @@ to_cds = df.groupby('year').count()
 
 source = ColumnDataSource(to_cds)
 
-p = figure(height=300, width=800, tools="xpan", toolbar_location=None,
+####
+
+p = figure(height=350, width=640, tools="xpan", toolbar_location=None,
            x_axis_type="datetime", x_axis_location="above",
-           background_fill_color="#efefef", x_range=(df['year'].min(), df['year'].max()))
+           background_fill_color="#efefef", x_range=(np.datetime64('1970-01-01'), np.datetime64('2020-01-01')))
 
 p.line('year', 'Key', source=source)
 p.yaxis.axis_label = 'Contagem'
 
 select = figure(title="Drag the middle and edges of the selection box to change the range above",
-                height=130, width=800, y_range=p.y_range,
+                height=130, width=640, y_range=p.y_range,
                 x_axis_type="datetime", y_axis_type=None,
                 tools="", toolbar_location=None, background_fill_color="#efefef")
 
@@ -42,5 +39,40 @@ select.line('year', 'Key', source=source)
 select.ygrid.grid_line_color = None
 select.add_tools(range_tool)
 
-show(column(p, select))
+####
+
+datetime = pd.to_datetime(df['Duration_ms'], unit='ms')
+
+df['Duration_s'] = (datetime.dt.minute)*60 + datetime.dt.second
+
+source = ColumnDataSource(df)
+
+p2 = figure(height=480, width=640, tools="xpan", toolbar_location=None,
+            x_axis_location="below", background_fill_color="#efefef",
+            x_range=(0, 0.5), y_range=(0, 500))
+
+p2.circle('Liveness', 'Duration_s', source=source)
+p2.yaxis.axis_label = 'Duração em segundos'
+p2.xaxis.axis_label = 'Speechiness'
+
+####
+
+df_views_por_track = df.sort_values(by = 'Views', ascending=False).drop_duplicates('Track')
+
+df_100_mais_vistos_completo = (df_views_por_track).head(100)
+
+df_100_mais_vistos_completo['Views'] = df_100_mais_vistos_completo['Views']/1000000
+
+source = ColumnDataSource(df_100_mais_vistos_completo)
+
+p3 = figure(height=480, width=640, toolbar_location='right',
+            x_axis_location="below", background_fill_color="#efefef")
+
+p3.circle('Liveness', 'Views', size=6, source=source)
+p3.yaxis.axis_label = 'Visualizações (em milhões)'
+p3.xaxis.axis_label = 'Liveness'
+
+select_layout = column(row(column(p, select), p2), p3)
+
+show(select_layout)
 
