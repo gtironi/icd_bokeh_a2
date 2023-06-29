925bb8175464c342bd8874c8cb4d5aa71f13a2b5
Luís Felipe Marciano <lfamarciano@gmail.com>
2023-06-28 21:14:17 -0300

    código comentado

--- visualizacoes/visualizacoes_marciano.py
+++ visualizacoes/visualizacoes_marciano.py
@@ -1,85 +1,98 @@
 # Arquivo para os códigos das  visualizacoes
 
+# Importando os módulos utilizados
 import pandas as pd
-from read_data import column_as_size
 from bokeh.plotting import figure
 from bokeh.io import output_file, show
 from bokeh.models.annotations import BoxAnnotation
 from bokeh.models import HoverTool, ColumnDataSource
 
 
-output_file("vis_marciano.html")
+########################################################################################################################
+
+data = pd.read_csv("visualizacoes/data/spotify_youtube_year.csv") # Lendo o .csv como um data frame do pandas
+
+########################################################################################################################
+# Primeiro gráfico: Scatter plot Liveness X Energy #
 
 
-# scatter plot speechiness X (outra variável) rascunho 
+color = [] # Criando lista vazia para posteriormente tranformar em coluna que define a cor dos glifos.
 
-# data = pd.read_csv("visualizacoes/data/spotify_youtube_year.csv")
+for each_float in data["Liveness"]: # Loop para percorrer a coluna Liveness, e dependendo se o valor é maior que 0.8
+     if each_float >= 0.8:             # é maior ou menor que 0.8, adiciona a palavra Red na lista vazia criada acima.
+         color.append("Red")
+     else:
+         color.append("Gray")
 
-# color = []
+data["color"] = color # Criando a coluna que irá definir a cor dos glifos baseada na lista criada acima
 
-# for each_float in data["Liveness"]:
-#     if each_float >= 0.8:
-#         color.append("Red")
-#     else:
-#         color.append("Gray")
+plot_1 = figure(width=600, height = 600, title = "Liveness X Energy") # Criando a figura do gráfico 2
 
-# data["color"] = color
+data_source_1 = ColumnDataSource(data) # Tranforma o data frame em ColumDataSource
 
-# plot_1 = figure(width=600, height = 600, title = "Circle Glyphs")
+plot_1.circle(x = "Liveness", y = "Energy", source = data_source_1, color = "color", alpha = 0.4, size = 8) #Criando scatter plot com os dados do data_source_1
 
-# data_source_1 = ColumnDataSource(data)
+box_annotation_4 = BoxAnnotation(left=0.8, right=1, fill_color = "Red", fill_alpha = 0.2) # Criando retângulo e dicionando ao gráfico
+plot_1.add_layout(box_annotation_4) 
 
-# plot_1.circle(x = "Liveness", y = "Energy", source = data_source_1, color = "color", alpha = 0.4, size = 8)
-
-# box_annotation_4 = BoxAnnotation(left=0.8, right=1, fill_color = "Red", fill_alpha = 0.2)
-# plot_1.add_layout(box_annotation_4)
-
-# tooltips = [
-#     ('Música', '@Track'),
-#     ('Artista', '@Artist'),
-#     ('Álbum', '@Album'),
-#     ('Streams', '@Stream')]
-
-# plot_1.add_tools(HoverTool(tooltips=tooltips))
+tooltips = [                       # Definindo as informações das músicas que aparecerão ao passar o mouse sobre os glifos
+    ('Música', '@Track'),
+    ('Artista', '@Artist'),
+    ('Álbum', '@Album'),
+    ('Streams', '@Stream')]
+plot_1.add_tools(HoverTool(tooltips=tooltips))
 
 
-# show(plot_1)
 
-######################################################################################
-# (variável) X tempo
+########################################################################################################################
+# Segundo gráfico: Duração das músicas X Anos
 
-plot_2 = figure()
+plot_2 = figure(width=600, height = 600) # Criando a figura do gráfico 1
+ 
+data["Duration_s"] = data["Duration_ms"]/1000 # Mudando a coluna Duration_ms para segundos (dividindo por 1000)
 
-data = pd.read_csv("visualizacoes/data/spotify_youtube_year.csv")
+duration_by_year = pd.DataFrame(data.groupby(["release_date"])["Duration_s"].mean()) # Agrupando por ano de lançamento e calculando a média de
+                                                                                     # duração das músicas por ano em um pd.Series transformado em DataFrame
 
-data["Duration_ms"] = data["Duration_ms"]/1000
-duration_by_year = pd.DataFrame(data.groupby(["release_date"])["Duration_ms"].mean())
-count_by_year = pd.DataFrame(data.groupby(["release_date"])["Track"].count())
-data_by_year = duration_by_year
+count_by_year = pd.DataFrame(data.groupby(["release_date"])["Track"].count())  # Agrupando por ano de lançamento e contando o número de
+                                                                               # músicas por ano em um pd.Series transformado em DataFrame
+
+
+data_by_year = duration_by_year                         # Linhas para juntar os DataFrames em um só
 data_by_year["Track Count"] = count_by_year["Track"]
 
-data_by_year_filtered = data_by_year[data_by_year['Track Count'] > 90]
+data_by_year_filtered = data_by_year[data_by_year['Track Count'] > 90] # Filtrando o DataFrame para os anos com mais de 90 músicas
 
-data_source_2 = ColumnDataSource(data_by_year_filtered)
+data_source_2 = ColumnDataSource(data_by_year_filtered) # Transformando em ColumnDataSource
 
-plot_2.line(x = "release_date", y = "Duration_ms", source = data_source_2)
+plot_2.line(x = "release_date", y = "Duration_s", source = data_source_2) # Criando o line plot com os dados do ColumnDataSource
 
-show(plot_2)
+tooltips = [                       # Definindo as informações das músicas que aparecerão ao passar o mouse sobre pontos da linha
+    ('Ano', '@release_date'),
+    ('Duração Média', '@Duration_s'),
+    ]
+plot_2.add_tools(HoverTool(tooltips=tooltips))
 
-######################################################################################
 
-# ranking de artistas rascunho
-# data = pd.read_csv("visualizacoes/data/spotify_youtube_year.csv")
 
-# data = pd.DataFrame(data.groupby(["Artist"])["Stream"].mean().sort_values(ascending=False).head(30))
+########################################################################################################################
 
-# data_source_3 = ColumnDataSource(data=dict(Artist=data.index, Stream=data.values))
+# Terceiro gráfico: Top 30 artistas
+data = data.dropna(subset=['Stream']) # Removendo as músicas que não possuiam o númeoro de streams
 
-# plot_3 = figure(y_range=data.index.tolist(), height=600, width=600, title="Top 30 Artistas Mais Ouvidos")
+stream_by_artist = pd.DataFrame(data.groupby(["Artist"])["Stream"].mean().sort_values().tail(30)) # Agrupando por artista, calulando a média de streams,
+                                                                                                  # ordenando da menor para a maior nº de streams e deixando
+                                                                                                  # somente as últimas 30 linhas do data frame.
 
-# plot_3.hbar(y='Artist', right='Stream', height=0.8, source=data_source_3)
+data_source_3 = ColumnDataSource(stream_by_artist) # Transformando em ColumnDataSource
 
-# plot_3.ygrid.grid_line_color = None
-# plot_3.yaxis.axis_label = "Artistas"
+plot_3 = figure(y_range=stream_by_artist.index.tolist(), height=600, width=600, title="Top 30 Artistas Mais Ouvidos") # Criando a figura do gráfico 3
 
-# show(plot_3)
+plot_3.hbar(y='Artist', right='Stream', height=0.8, source=data_source_3) # Criando o horizontal bar plot com os dados co ColumnDataSource
+
+tooltips = [                       # Definindo as informações das músicas que aparecerão ao passar o mouse sobre as barras
+    ('Nº Médio de Streams', '@Stream'),
+    ]
+plot_3.add_tools(HoverTool(tooltips=tooltips))
+
+
