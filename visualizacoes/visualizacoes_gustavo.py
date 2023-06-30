ab9b8e536ef280a30ed7381faf81bf05a83e44b6
Gustavo Tironi <b51083@fgv.edu.br>
2023-06-30 20:05:34 -0300

    Criei uma função com o layout

--- visualizacoes/visualizacoes_gustavo.py
+++ visualizacoes/visualizacoes_gustavo.py
@@ -89,35 +89,41 @@ def plot_3_gustavo(dataframe):
 
 p3 = plot_3_gustavo(df)
 
-text1 = Div(text=""" <h2>Gráfico de linha</h2>
-<p>Ao lado, foi plotado um gráfico de linha, o qual demonstra a quantidade de músicas, no dataset, lançadas por ano.<br>
-Para sua execução, o dataset foi agrupado por ano, então foi contado a quantidade de ocorrências e utilizando o 
-método <b>line()</b> para fazer o plot principal.<br>
-Ainda, foi adicionado uma barra de rolagem abaixo do gráfico, que permite rolar o gráfico. Essa barra usa a ferramenta
-<b>RangeTool</b> e a conexão entre o plot e barra de rolagem, para gerar o efeito apresentado.<p>
-""", 
-styles = {'text-align': 'center', 'font-size': '16px'}, width=430, align = 'center')
+def cria_layout_gustavo():
+    
+    plot_1 = plot_1_gustavo(source)
+    plot_2 = plot_2_gustavo(source1, 'Acousticness')
+    plot_3 = plot_3_gustavo(df)
 
-text2 = Div(text=""" <h2>Gráfico de dispersão</h2>
-<p>Acima, foi plotado um gráfico de dispersão, dessa vez, considerando apenas as <b>100 músicas mais visualizadas</b><br>
-no YouTube. Esse gráfico demonstra uma referência acústica (Acousticness) vs a quantidade de visualizações de cada música. 
-Além disso, ao colocar o mouse em cima de um dos pontos, é possível ver o <b>nome da música</b>, o <b>artista</b>, a 
-<b>quantidade de visualizações</b> e a <b>URL</b>, que leva à página da música em questão no YouTube.<br>
-Para sua execução, o dataset foi filtrado, sobrando apenas as 100 músicas com mais views, e então utilizado o método 
-<b>circle()</b> para fazer o plot.<p>
-""",
-styles = {'text-align': 'center', 'font-size': '16px'}, width=580, margin=(0, 40, 50, 40))
+    text1 = Div(text=""" <h2>Gráfico de linha</h2>
+    <p>Ao lado, foi plotado um gráfico de linha, o qual demonstra a quantidade de músicas, no dataset, lançadas por ano.<br>
+    Para sua execução, o dataset foi agrupado por ano, então foi contado a quantidade de ocorrências e utilizando o 
+    método <b>line()</b> para fazer o plot principal.<br>
+    Ainda, foi adicionado uma barra de rolagem abaixo do gráfico, que permite rolar o gráfico. Essa barra usa a ferramenta
+    <b>RangeTool</b> e a conexão entre o plot e barra de rolagem, para gerar o efeito apresentado.<p>
+    """, 
+    styles = {'text-align': 'center', 'font-size': '16px'}, width=430, align = 'center')
 
-text3 = Div(text=""" <h2>Gráfico Boxplot</h2>
-<p>Acima, foi plotado um <b>boxplot</b>, da popularidade das músicas, conforme a existência de um videoclipe.<br>
-Esse gráfico demonstra a distribuição dos valores em cada categoria, bem como identifica os <b>outliers</b>. 
-Para sua execução, foi definida uma função - baseada na documentação do bokeh - que monta cada parte do boxplot
-individualmente. Essa função, calcula todos os valores necessários, e plota cada elemento do boxplot a partir disso.<br>
-Superficialmente, observando o boxplot, podemos dizer que há um indicativo de maior popularidade em
-músicas com videoclipe.
-""",
-styles = {'text-align': 'center', 'font-size': '16px'}, width=580, margin=(0, 40, 50, 40))
+    text2 = Div(text=""" <h2>Gráfico de dispersão</h2>
+    <p>Acima, foi plotado um gráfico de dispersão, dessa vez, considerando apenas as <b>100 músicas mais visualizadas</b><br>
+    no YouTube. Esse gráfico demonstra uma referência acústica (Acousticness) vs a quantidade de visualizações de cada música. 
+    Além disso, ao colocar o mouse em cima de um dos pontos, é possível ver o <b>nome da música</b>, o <b>artista</b>, a 
+    <b>quantidade de visualizações</b> e a <b>URL</b>, que leva à página da música em questão no YouTube.<br>
+    Para sua execução, o dataset foi filtrado, sobrando apenas as 100 músicas com mais views, e então utilizado o método 
+    <b>circle()</b> para fazer o plot.<p>
+    """,
+    styles = {'text-align': 'center', 'font-size': '16px'}, width=580, margin=(0, 40, 50, 40))
 
-select_layout = column(row(p1, text1), row(column(p2, text2), column(p3, text3)))
+    text3 = Div(text=""" <h2>Gráfico Boxplot</h2>
+    <p>Acima, foi plotado um <b>boxplot</b>, da popularidade das músicas, conforme a existência de um videoclipe.<br>
+    Esse gráfico demonstra a distribuição dos valores em cada categoria, bem como identifica os <b>outliers</b>. 
+    Para sua execução, foi definida uma função - baseada na documentação do bokeh - que monta cada parte do boxplot
+    individualmente. Essa função, calcula todos os valores necessários, e plota cada elemento do boxplot a partir disso.<br>
+    Superficialmente, observando o boxplot, podemos dizer que há um indicativo de maior popularidade em
+    músicas com videoclipe.
+    """,
+    styles = {'text-align': 'center', 'font-size': '16px'}, width=580, margin=(0, 40, 50, 40))
+    
+    select_layout = column(row(plot_1, text1), row(column(plot_2, text2), column(plot_3, text3)))
 
-show(select_layout)
+    return select_layout
