5288d4bf07bbda94ea9189e6eada61b387e60f4c
Sillas Rocha <125564261+scrocha@users.noreply.github.com>
2023-07-01 01:49:53 -0300

    adicionei os gráficos do servidor bokeh
    agora os gráficos do servidor bokeh estão disponíveis na plotagem, possuem interatividade mas seu carregamento é um pouco lento

--- html_estatico_final.py
+++ html_estatico_final.py
@@ -11,12 +11,13 @@ from visualizacoes.visualizacoes_leonardo import gera_layout_leonardo
 def gera_html(path):
 
     output_file("index.html")
-
-    layout_sillas = gera_layout_estatico_sillas(path)
+    
     layout_marciano = cria_layout_marciano(path)
     layout_gustavo = cria_layout_gustavo(path)
-    layout_leonardo = gera_layout_leonardo()
-        
+    layout_leonardo = gera_layout_leonardo(path)
+    layout_sillas =  Div(text="<iframe src='https://bokeh-server-aad0ecb16fba.herokuapp.com/app' width='1500px' height='1637px' scrolling='no' style='border: none;'></iframe>")
+    
+
     home_page_text = """<h3 id="trabalho-de-introdu-o-ci-ncia-de-dados-">Trabalho de Introdução à ciência de dados:</h3>
     <p>Os alunos Leonardo Alexandre, Sillas Rocha, Gustavo Tironi e Luís Felipe elaboraram visualizações a partir de uma base e dados, encontrada no Kaggle, sobre músicas do Spotify e do YouTube.
     A base de dados possui diversas variáveis, como autor, nome, data de lançamento e duração.
@@ -34,17 +35,17 @@ def gera_html(path):
     </ul>
     """
 
-    espaço_sillas = Div(text = 'Gráficos Sillas', height=120, style = {'text-align': 'center', 'font-size': '70px'}, margin=(40, 0, 40, 0), align = 'center')
-
     espaço_gustavo = Div(text = 'Gráficos Gustavo', height=120, style = {'text-align': 'center', 'font-size': '70px'}, margin=(40, 0, 40, 0), align = 'center')
 
     espaço_marciano = Div(text = 'Gráficos Marciano', height=120, style = {'text-align': 'center', 'font-size': '70px'}, margin=(40, 0, 40, 0), align = 'center')
 
-    espaço_leonardo = Div(text = 'Gráficos leonardo', height=120, style = {'text-align': 'center', 'font-size': '70px'}, margin=(40, 0, 40, 0), align = 'center')
+    espaço_leonardo = Div(text = 'Gráficos Leonardo', height=120, style = {'text-align': 'center', 'font-size': '70px'}, margin=(40, 0, 40, 0), align = 'center')
+    
+    espaço_sillas = Div(text = 'Gráficos Sillas (Servidor Bokeh)', height=120, style = {'text-align': 'center', 'font-size': '70px'}, margin=(40, 0, 40, 0), align = 'center')
 
     home_page_div = Div(text = home_page_text,
-                            style = {'text-align': 'justify', 'font-size': '16px'}, width=580, margin=(0, 0, 0, 450))
+                            style = {'text-align': 'justify', 'font-size': '16px'}, width=580, margin=(0, 40, 50, 40))
 
-    main_page = column(home_page_div, espaço_sillas, layout_sillas, espaço_marciano, layout_marciano, espaço_gustavo, layout_gustavo, espaço_leonardo, layout_leonardo)
+    main_page = column(home_page_div, espaço_marciano, layout_marciano, espaço_gustavo, layout_gustavo, espaço_leonardo, layout_leonardo, espaço_sillas, layout_sillas)
 
     return main_page
